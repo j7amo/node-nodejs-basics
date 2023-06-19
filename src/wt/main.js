3f0837deab94e7b4a6ca115920ed40053cc9530b
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 17:04:06 +0300

    implement worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
     // Write your code here
+    const startingNumber = 10;
+    const cpusNumber = cpus().length;
+    const workerPromises = [];
+
+    for (let i = startingNumber; i < startingNumber + cpusNumber; i++) {
+        const worker = new Worker('./worker.js');
+
+        const workerPromise = new Promise((resolve, reject) => {
+            worker
+                .on('message', (message) => {
+                    worker.terminate();
+                    resolve({ status: 'resolved', data: message });
+                })
+                .on('error', (err) => reject({ status: 'error', data: null }));
+        });
+
+        workerPromises.push(workerPromise);
+        worker.postMessage(i);
+    }
+
+    const results = await Promise.all(workerPromises);
+
+    for (let result of results) {
+        console.log(result);
+    }
 };
 
 await performCalculations();
