beb646f7fbe1960e99e08e15f05e659fbf2b4285
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 14:09:24 +0300

    implement write.js (streams)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await pipeline(
+            process.stdin,
+            createWriteStream('./files/fileToWrite.txt')
+        );
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await write();
