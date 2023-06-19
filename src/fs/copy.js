8ccb49f41ae7bc9bd3bd04440a9fa27fda69466b
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 07:57:36 +0300

    implement copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { promises as fsPromises } from "fs";
+
+const checkIfPathAlreadyExists = async (relativePath) => {
+    try {
+        const stat = await fsPromises.stat(relativePath);
+
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    const isSourceFolderExists = await checkIfPathAlreadyExists("./files");
+    const isDestinationFolderExists = await checkIfPathAlreadyExists("./files_copy");
+
+    if (!isSourceFolderExists || isDestinationFolderExists) {
+        throw new Error("FS operation failed");
+    }
+
+    try {
+        await fsPromises.cp("./files", "./files_copy", { recursive: true });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await copy();
