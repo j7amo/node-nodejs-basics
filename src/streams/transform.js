e56142ca9b1d873855e1d67aba927d359e9d5189
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 14:40:54 +0300

    implement transform.js (streams)

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { pipeline } from 'stream/promises';
+import { TransformStream } from 'stream/web';
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const transform = new TransformStream({
+        transform(chunk, controller) {
+            controller.enqueue(`${chunk.toString().trim().split('').reverse().join('')}\n`);
+        }
+    });
+
+    try {
+        await pipeline(
+            process.stdin,
+            transform,
+            process.stdout
+        );
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await transform();
