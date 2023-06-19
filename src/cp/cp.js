f6242e00fd688bf36e85d60193c72833989f7160
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 19:49:46 +0300

    implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
-const spawnChildProcess = async (args) => {
+import { exec } from 'child_process';
+
+const spawnChildProcess = async (...args) => {
     // Write your code here
+    const childProcess = exec(`node ./files/script.js ${args.join(' ')}`, (err, stdout, stdin) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+        }
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(1, 2, 3);
