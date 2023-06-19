fb075180d6e14eddb0d14796f0a780a2e780e44a
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 15:17:01 +0300

    implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await pipeline(
+            createReadStream('./files/archive.gz'),
+            createUnzip(),
+            createWriteStream('./files/fileToCompress.txt')
+        );
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await decompress();
