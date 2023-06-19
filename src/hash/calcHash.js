96bb2026a71d96674197f368db78144fa5d2df22
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 13:07:57 +0300

    implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { promises as fsPromises } from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const fileContent = await fsPromises.readFile('./files/fileToCalculateHashFor.txt', { encoding: 'utf8' });
+        const hash = createHash('sha256');
+        hash.update(fileContent);
+        console.log(hash.digest('hex'));
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await calculateHash();
