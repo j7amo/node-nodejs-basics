5dd1f6fd686027a98b199c169fdd2b5bcae6cda5
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 10:21:45 +0300

    implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    for (let i = 2; i < process.argv.length; i++) {
+        if (process.argv[i].startsWith("--prop")) {
+            console.log(`${process.argv[i].replace("--", "")} is ${process.argv[i + 1]}`);
+        }
+    }
 };
 
 parseArgs();
