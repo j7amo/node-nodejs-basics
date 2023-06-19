527b06dd6d4f7338e0e71b9f7576e0a7a668aea9
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-19 14:52:42 +0300

    implement compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await pipeline(
+            createReadStream('./files/fileToCompress.txt'),
+            createGzip(),
+            createWriteStream('./files/archive.gz')
+        );
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await compress();
